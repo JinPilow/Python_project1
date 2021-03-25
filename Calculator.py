24cbe44380a606d05adee4a608a005bec4b650fe
JinPilow <80875572+JinPilow@users.noreply.github.com>
2021-03-25 15:06:53 +0900

    계산기 draft1

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,38 @@
-a = int(input("숫자를 입력하시오"))
 
-b = int(input("숫자를 입력하시오"))
+a = []
+
+for i in range(1,100):
+    b = input("계산식을 입력하시오")
+    if b == "=":
+        break
+    else:
+        a.append(b)
+print(a)
 
 from lib.Math import *
 
-result= sum(a,b)
-result1= div(a,b)
-print(result)
-print(result1)
+for i in range(len(a)):
+    if '*' in a:
+        b = a.index("*")
+        a[b] = mul(int(a[b-1]), int(a[b+1]))
+        a.remove(a[b+1])
+        a.remove(a[b-1])
+    elif '/' in a:
+        b = a.index("/")
+        a[b] = div(int(a[b-1]), int(a[b+1]))
+        a.remove(a[b+1])
+        a.remove(a[b-1])
+    elif '-' in a:
+        b = a.index("-")
+        a[b] = sub(int(a[b-1]), int(a[b+1]))
+        a.remove(a[b+1])
+        a.remove(a[b-1])
+    elif '+' in a:
+        b= a.index("+")
+        a[b] = add(int(a[b-1]), int(a[b+1]))
+        a.remove(a[b+1])
+        a.remove(a[b-1])
+    else:
+        pass
 
+print("Result: {}".format(a))
