1ec811e6fad9e777eba5868b9f85bfc140536976
JinPilow <80875572+JinPilow@users.noreply.github.com>
2021-03-26 13:15:20 +0900

    변수 이름 변경

--- Calculator.py
+++ Calculator.py
@@ -1,38 +1,47 @@
 
-a = []
+formula = []
 
-for i in range(1,100):
-    b = input("계산식을 입력하시오")
-    if b == "=":
+while True:
+    term = input("계산식을 입력하시오")
+    if term == "=":
         break
-    else:
-        a.append(b)
-print(a)
+    formula.append(term)
+    if formula[0].isnumeric() == False:
+        print("잘못된 식입니다.")
+        quit()
+    for i in range(len(formula)):
+        if formula[i].isnumeric == False and formula[i+1].isnumeric == False:
+            print("잘못된 식입니다.")
+
+print(formula)
 
 from lib.Math import *
 
-for i in range(len(a)):
-    if '*' in a:
-        b = a.index("*")
-        a[b] = mul(int(a[b-1]), int(a[b+1]))
-        a.remove(a[b+1])
-        a.remove(a[b-1])
-    elif '/' in a:
-        b = a.index("/")
-        a[b] = div(int(a[b-1]), int(a[b+1]))
-        a.remove(a[b+1])
-        a.remove(a[b-1])
-    elif '-' in a:
-        b = a.index("-")
-        a[b] = sub(int(a[b-1]), int(a[b+1]))
-        a.remove(a[b+1])
-        a.remove(a[b-1])
-    elif '+' in a:
-        b= a.index("+")
-        a[b] = add(int(a[b-1]), int(a[b+1]))
-        a.remove(a[b+1])
-        a.remove(a[b-1])
+for i in range(len(formula)):
+    if '*' in formula:
+        sign = formula.index("*")
+        formula[sign] = mul(int(formula[sign-1]), int(formula[sign+1]))
+        formula.remove(formula[sign+1])
+        formula.remove(formula[sign-1])
+        print(formula)
+    elif '/' in formula:
+        sign = formula.index("/")
+        formula[sign] = div(int(formula[sign-1]), int(formula[sign+1]))
+        formula.remove(formula[sign+1])
+        formula.remove(formula[sign-1])
+        print(formula)
+    elif '-' in formula:
+        sign = formula.index("-")
+        formula[sign] = sub(int(formula[sign-1]), int(formula[sign+1]))
+        formula.remove(formula[sign+1])
+        formula.remove(formula[sign-1])
+        print(formula)
+    elif '+' in formula:
+        sign = formula.index("+")
+        formula[sign] = add(int(formula[sign-1]), int(formula[sign+1]))
+        formula.remove(formula[sign+1])
+        formula.remove(formula[sign-1])
+        print(formula)
     else:
         pass
-
-print("Result: {}".format(a))
+print("Result: {}".format(formula))
