c16c04a390e2132bcbb5a70bc1ec182ffe4ca16a
jmhj456456 <50853896+jmhj456456@users.noreply.github.com>
2021-04-05 12:39:56 +0900

    Update gCalculator.py

--- gCalculator.py
+++ gCalculator.py
@@ -38,19 +38,13 @@ def onclick(text):
     elif text == "AC":
         inputs.delete(0, END)
     elif text == "C":
-        inputs.delete(len(Entry.get(inputs))-1, END)
+        inputs.delete(len(Entry.get(inputs)) - 1, END)
     else:
         tinput = Entry.get(inputs) + text
         inputs.delete(0, END)
         inputs.insert(0, tinput)
 
 
-# print("1 : 한번에 입력\n2 : 하나씩 입력\n")
-# while 1:
-#     mode = input("모드 : ")
-#
-#     # 한번에 입력 계산기
-#     if mode == '1':
 def calc(s):
     str = s
 
@@ -61,24 +55,24 @@ def calc(s):
         temp += str[0]
         str = str[1:]
     if str[0].isdigit():  # 숫자로 시작하는지 확인
-            for i in range(len(str)):
-                if str[i].isdigit() or str[i] == ".":  # 숫자확인후 temp에 저장
-                    temp += str[i]
-                elif issign(str[i]):  # 부호일 경우 temp를 number에 저장하고 부호는 sign에 저장
-                    number.append(temp)
-                    temp = ""
-                    sign.append(str[i])
-                    if issign(str[i+1]):
-                        inputs.delete(0, END)
-                        inputs.insert(0, "연산자가 연속으로 나옵니다!")
-                        return
-                else:  # 그 의외의 문자면 경고 출력후 프로그램 종료
+        for i in range(len(str)):
+            if str[i].isdigit() or str[i] == ".":  # 숫자확인후 temp에 저장
+                temp += str[i]
+            elif issign(str[i]):  # 부호일 경우 temp를 number에 저장하고 부호는 sign에 저장
+                number.append(temp)
+                temp = ""
+                sign.append(str[i])
+                if issign(str[i + 1]):
                     inputs.delete(0, END)
-                    inputs.insert(0, "숫자와 사칙연산만 입력해주세요!")
-                    print("숫자와 사칙연산만 입력해주세요!")
+                    inputs.insert(0, "연산자가 연속으로 나옵니다!")
                     return
-                if i + 1 == len(str):  # 마지막 숫자 number에 저장
-                    number.append(temp)
+            else:  # 그 의외의 문자면 경고 출력후 프로그램 종료
+                inputs.delete(0, END)
+                inputs.insert(0, "숫자와 사칙연산만 입력해주세요!")
+                print("숫자와 사칙연산만 입력해주세요!")
+                return
+            if i + 1 == len(str):  # 마지막 숫자 number에 저장
+                number.append(temp)
     else:  # 예외처리
         inputs.delete(0, END)
         inputs.insert(0, "숫자로 시작해 주세요!")
@@ -106,54 +100,6 @@ def calc(s):
     print("답은 : {}".format(number[0]))
     inputs.delete(0, "end")
     inputs.insert(0, number[0])
-
-
-    # 하나씩 입력 계산기
-    # elif mode == '2':
-    #
-    #     print("(=까지 입력)")
-    #
-    #     str = []  # 자료 저장 리스트
-    #     temp = ''  # 문자 임시저장소
-    #
-    #     test = True  # 문자, 숫자 확인 스위치(참 = 숫자검사, 거짓 = 부호검사)
-    #     while True:
-    #         temp = input(" >")
-    #         if temp == '=':  # = 가 나오기전까지 입력 받기
-    #             break
-    #         else:
-    #             if test:
-    #                 if temp.isdigit():  # 숫자 확인
-    #                     str.append(temp)
-    #                     test = False
-    #                 else:
-    #                     print("숫자를 입력하세요!")
-    #             else:
-    #                 if issign(temp):  # 부호 확인
-    #                     str.append(temp)
-    #                     test = True
-    #                 else:
-    #                     print("부호를 입력하세요!")
-    #
-    #     count = 0
-    #     while count < len(str):
-    #         if str[count] == '*' or str[count] == '/':
-    #             function2(str, count)  # 계산 함수
-    #         else:
-    #             count += 1
-    #
-    #     count = 0
-    #     while count < len(str):
-    #         if str[count] == '+' or str[count] == '-':
-    #             function2(str, count)  # 계산 함수
-    #         else:
-    #             count += 1
-    #
-    #     print("답은 : {}".format(str[0]))
-    #     quit()
-    #
-    # else:
-    #     print("모드를 잘못 입력하였습니다!")
 
 
 calculator.mainloop()
