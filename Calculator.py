a78d2dabae468d9b64e9eed524e3f6735f3feadb
JinPilow <80875572+JinPilow@users.noreply.github.com>
2021-06-08 18:53:36 +0900

    계산기 수정

--- Calculator.py
+++ Calculator.py
@@ -2,35 +2,78 @@ from tkinter import *
 from tkinter import messagebox
 from lib.Math import *
 
-root = Tk()
+class Main:
+    def __init__(self, tk):
+        self.calc = Function()
 
-root.title("Calculator")
-root.geometry("400x550+500+100")
-root.resizable(0, 0)
+class Interface():
+    def layout(self):
+        self.entry = Entry(upper_frame, justify = "right", width=20, font=("Courier", 18), borderwidth=5)
+        self.entry.pack()
+        self.entry.insert(0, "0")
 
-upper_frame = Frame(root, width=400, height=70)
-upper_frame.pack(pady=40)
+        # 숫자 버튼 설정
+        bnum = []
+        for number in range(10):  # 숫자 버튼 1~9 생성
+            button_num = Button(down_frame, text=str(number), font=("Courier", 18), padx=15, pady=10,
+                             command=lambda number=number: self.click(number))
+            bnum.append(button_num)
+        bnum.append(Button(down_frame, text="00", font=("Courier", 18), padx=9, pady=10,
+                           command=lambda: self.click('00')))  # 숫자 버튼 00 생성
+        bnum.append(Button(down_frame, text=".", font=("Courier", 18), padx=15, pady=10,
+                           command=lambda: self.click('.')))  # 숫자 버튼 소수점 '.' 생성
 
-down_frame = Frame(root, width=400, height=100)
-down_frame.pack(padx=10, pady=10)
+        countnum = 1
+        for row in range(3):  # 숫자 버튼 gui 위치 지정
+            for column in range(3):
+                bnum[countnum].grid(row=2 - row, column=column, padx=5, pady=5)
+                countnum += 1
 
-entry = Entry(upper_frame, width=20, font=("Courier",18), borderwidth=5)
-entry.pack()
-entry.insert(0,"")
+        bnum[0].grid(row=3, column=1, padx=5, pady=5)
+        bnum[10].grid(row=3, column=0, padx=5, pady=5)
+        bnum[11].grid(row=3, column=2, padx=5, pady=5)
 
-class Function:
+        # 부호 버튼 설정
+        bsign = []
+        for sign in ["*", "/", "+", "-"]:  # 부호 버튼 "*","/","+","-" 생성
+            button_sign = Button(down_frame, text=sign, font=("Courier", 18), padx=15, pady=10,
+                             command=lambda sign=sign: self.click(sign))
+            bsign.append(button_sign)
+
+        bsign.append(Button(down_frame, text="=", font=("Courier", 18), padx=15, pady=10,
+                            command=lambda: self.equal()))  # 부호 버튼 "=" 생성
+        bsign.append(Button(down_frame, text="C", font=("Courier", 18), padx=15, pady=10,
+                            command=lambda: self.clear()))  # 부호 버튼 클리어 "C" 생성
+        bsign.append(Button(down_frame, text="AC", font=("Courier", 18), padx=8, pady=10,
+                            command=lambda: self.all_clear()))  # 부호 버튼 올클리어 "AC" 생성
+        bsign.append(Button(down_frame, text="+/-", font=("Courier", 18), padx=2, pady=10,
+                            command=lambda: self.switch_sign()))  # 부호 버튼 부호 변환 "+/-" 생성
+
+        countsign = 0
+        for row in range(5):  # 부호 버튼 gui 위치 지정
+            bsign[countsign].grid(row=row, column=3, padx=5, pady=5)
+            countsign += 1
+
+        bsign[4].grid(row=4, column=3, padx=5, pady=5)
+        bsign[5].grid(row=4, column=2, padx=5, pady=5)
+        bsign[6].grid(row=4, column=1, padx=5, pady=5)
+        bsign[7].grid(row=4, column=0, padx=5, pady=5)
+
+
+class Function(Interface):
     def __init__(self):
+        self.layout()
         self.count = 0
-        Function.current = entry.get()
-        self.result = False
+        self.result = True
 
-    def b_click(self, n):
+    # 클릭 함수 생성
+    def click(self, n):
         if self.result and str(n).isdigit():
-            entry.delete(0, END)
+            self.entry.delete(0, END)
             self.count = 0
-        Function.current = entry.get()
-        entry.delete(0, END)
-        entry.insert(0, str(Function.current) + str(n))
+        current = self.entry.get()
+        self.entry.delete(0, END)
+        self.entry.insert(0, str(current) + str(n))
         self.result = False
         if str(n) == "00":
             self.count += 2
@@ -42,23 +85,27 @@ class Function:
             self.count = 0
             print(self.count)
 
-    def b_clear(self):
-        Function.current = entry.get()
-        temp = list(Function.current)
-        del temp[-1]
-        Function.current = ''.join(temp)
-        entry.delete(0, END)
-        entry.insert(0, Function.current)
+    # 클리어 함수 생성
+    def clear(self):
+        current = self.entry.get()
+        # temp = list(current)
+        # del temp[-1]
+        # current = ''.join(temp)
+        current = current[:-1]
+        self.entry.delete(0, END)
+        self.entry.insert(0, current)
         self.count -= 1
 
-    def b_allclear(self):
-        entry.delete(0,END)
+    # 올클리어 함수 생성
+    def allclear(self):
+        self.entry.delete(0,END)
         self.count = 0
         self.result = False
 
+    # 부호 변환 함수 생성
     def switch_sign(self):
-        Function.current = entry.get()
-        temp = Function.current[-self.count-1:]
+        current = self.entry.get()
+        temp = current[-self.count-1:]
         if not temp[0].isdigit():
             if len(temp) == 1:
                 messagebox.showinfo("Error", "기능을 수행할 수 없습니다.")
@@ -73,47 +120,48 @@ class Function:
                 currentre = self.current.replace(self.current[-self.count:], "-" + self.current[-self.count:])
             else:
                 currentre = 0
-        entry.delete(0, END)
-        entry.insert(0, currentre)
+        self.entry.delete(0, END)
+        self.entry.insert(0, currentre)
 
+    # 등호(계산) 함수 생성
     def equal(self):
-        Function.current = entry.get()
+        current = self.entry.get()
         formula = []
         temp = ""
         i = 0
 
-        if Function.current[0] == "-":          #계산식 첫 글자가 "-"일 때 배열에 "0", "-"를 추가하여 음수로 연산
+        if current[0] == "-":          #계산식 첫 글자가 "-"일 때 배열에 "0", "-"를 추가하여 음수로 연산
             formula.extend(["0", "-"])
             i = 1
-        elif Function.current[0] == "+":        #계산식 첫 글자가 "+"일 때 배열에 "0", "+"를 추가하여 양수로 연산
+        elif current[0] == "+":        #계산식 첫 글자가 "+"일 때 배열에 "0", "+"를 추가하여 양수로 연산
             formula.extend(["0", "+"])
             i = 1
-        elif Function.current[0] == ".":        #계산식 첫 글자가 "."일 때 pass
+        elif current[0] == ".":        #계산식 첫 글자가 "."일 때 pass
             pass
-        elif not Function.current[0].isdigit(): #계산식 첫 글자가 그 외의 문자일 때 에러 메세지 출력
+        elif not current[0].isdigit(): #계산식 첫 글자가 그 외의 문자일 때 에러 메세지 출력
             messagebox.showinfo("Error", "연산을 수행할 수 없습니다.")
         while True:
-            if is_digit(Function.current[i]): #첫번째 이외 글자가 숫자이거나 소수점일 때 모든 문자를 문자열에 저장
-                temp += Function.current[i]
+            if is_digit(current[i]): #첫번째 이외 글자가 숫자이거나 소수점일 때 모든 문자를 문자열에 저장
+                temp += current[i]
                 i += 1
-            elif not Function.current[i].isdigit():         #첫번째 이외 글자가 부호일 때 문자열에 저장된 숫자들을 배열에 추가하고 부호도 배열에 추가
-                if Function.current[i + 1] in ["+", "-"]:   #부호 두 개 연달아 입력시 나중에 입력된 부호가 "+" 또는 "-"일 경우 문자열에 숫자와 함께 저장
+            elif not current[i].isdigit():         #첫번째 이외 글자가 부호일 때 문자열에 저장된 숫자들을 배열에 추가하고 부호도 배열에 추가
+                if current[i + 1] in ["+", "-"]:   #부호 두 개 연달아 입력시 나중에 입력된 부호가 "+" 또는 "-"일 경우 문자열에 숫자와 함께 저장
                     formula.append(temp)
-                    formula.append(Function.current[i])
+                    formula.append(current[i])
                     temp = ''
-                    temp += Function.current[i + 1]
+                    temp += current[i + 1]
                     i += 2
-                elif Function.current[i + 1] in ["*", "/"]: #부호 두 개 연달아 입력시 나중에 입력된 부호가 "*" 또는 "/"일 경우 오류 메세지 출력
+                elif current[i + 1] in ["*", "/"]: #부호 두 개 연달아 입력시 나중에 입력된 부호가 "*" 또는 "/"일 경우 오류 메세지 출력
                     i += 1
                     messagebox.showinfo("Error", "잘못된 연산자입니다.")
-                    entry.delete(0, END)
+                    self.entry.delete(0, END)
                 else:
                     formula.append(temp)
-                    formula.append(Function.current[i])
+                    formula.append(current[i])
                     temp = ''
                     i += 1
-            if i == len(Function.current) - 1:              #마지막 숫자를 배열에 입력
-                temp += Function.current[i]
+            if i == len(current) - 1:              #마지막 숫자를 배열에 입력
+                temp += current[i]
                 formula.append(temp)
                 print(formula)
                 break
@@ -122,7 +170,7 @@ class Function:
             if i%2 == 0:
                 if not is_digit(formula[i]):
                     messagebox.showinfo("Error", "잘못된 연산자입니다.")
-                    entry.delete(0, END)
+                    self.entry.delete(0, END)
         try:
             i = 0
             while len(formula) > 1:
@@ -144,58 +192,24 @@ class Function:
                         i = 0
                     else:
                         i += 1                      # i 번째 입력값이 숫자일 경우 i 값에 1을 더해 다음 입력값 검사
-            entry.delete(0, END)
-            entry.insert(0, formula[0])
+            self.entry.delete(0, END)
+            self.entry.insert(0, formula[0])
         except IndexError as e:
              print(e)
 
-#숫자 버튼 설정
-bnum = []
-func = Function()
+# 계산기 프레임
+root = Tk()
 
-for number in range(10):                                    #숫자 버튼 1~9 생성
-    button1 = Button(down_frame, text=str(number), font=("Courier",18), padx = 15, pady = 10,
-                     command = lambda number=number: func.b_click(number))
-    bnum.append(button1)
-bnum.append(Button(down_frame, text="00", font=("Courier",18), padx = 9, pady = 10,
-                   command = lambda: func.b_click('00')))   #숫자 버튼 00 생성
-bnum.append(Button(down_frame, text=".", font=("Courier",18), padx = 15, pady = 10,
-                   command = lambda: func.b_click('.')))    #숫자 버튼 소수점 '.' 생성
+root.title("Calculator")
+root.geometry("400x550+500+100")
+root.resizable(0, 0)
 
-countnum = 1
-for row in range(3):                                        #숫자 버튼 gui 위치 지정
-    for column in range(3):
-        bnum[countnum].grid(row = 2-row,column = column, padx = 5, pady = 5)
-        countnum += 1
+upper_frame = Frame(root, width=400, height=70)
+upper_frame.pack(pady=40)
 
-bnum[0].grid(row = 3, column = 1, padx = 5, pady = 5)
-bnum[10].grid(row = 3, column = 0, padx = 5, pady = 5)
-bnum[11].grid(row = 3, column = 2, padx = 5, pady = 5)
+down_frame = Frame(root, width=400, height=100)
+down_frame.pack(padx=10, pady=10)
 
-#부호 버튼 설정
-bsign = []
-for sign in ["*","/","+","-"]:                              #부호 버튼 "*","/","+","-" 생성
-    button2 = Button(down_frame, text= sign, font=("Courier",18), padx = 15, pady = 10,
-                     command = lambda sign=sign: func.b_click(sign))
-    bsign.append(button2)
-
-bsign.append(Button(down_frame, text= "=", font=("Courier",18), padx = 15, pady = 10,
-                    command = lambda: func.equal()))        #부호 버튼 "=" 생성
-bsign.append(Button(down_frame, text= "C", font=("Courier",18), padx = 15, pady = 10,
-                    command = lambda: func.b_clear()))      #부호 버튼 클리어 "C" 생성
-bsign.append(Button(down_frame, text= "AC", font=("Courier",18), padx = 8, pady = 10,
-                    command = lambda: func.b_allclear()))   #부호 버튼 올클리어 "AC" 생성
-bsign.append(Button(down_frame, text= "+/-", font=("Courier",18), padx = 2, pady = 10,
-                    command = lambda: func.switch_sign()))  #부호 버튼 플러스 마이너스 변환 "+/-" 생성
-
-countsign = 0
-for row in range(5):                                        #부호 버튼 gui 위치 지정
-    bsign[countsign].grid(row = row,column = 3, padx = 5, pady = 5)
-    countsign += 1
-
-bsign[4].grid(row = 4, column = 3, padx = 5, pady = 5)
-bsign[5].grid(row = 4, column = 2, padx = 5, pady = 5)
-bsign[6].grid(row = 4, column = 1, padx = 5, pady = 5)
-bsign[7].grid(row = 4, column = 0, padx = 5, pady = 5)
+Main(root)
 
 root.mainloop()
